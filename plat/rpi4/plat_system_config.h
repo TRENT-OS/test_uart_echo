df400ee3a5286a3b8a8aa286827a3d5cd602b76f
Felix Schladt <felix.schladt@hensoldt.net>
2024-03-15 10:13:18 +0100

    [DEV] Add RPI4 Platform

---
+++ plat/rpi4/plat_system_config.h
@@ -0,0 +1,10 @@
+/*
+ *  Platform defaults for the Raspberry Pi 4.
+ *
+ *  Copyright (C) 2024, HENSOLDT Cyber GmbH
+ *
+*/
+
+
+// kernel log uses UART_1, so we can use UART_2 for i/o test
+#define UART_IO     UART_2
